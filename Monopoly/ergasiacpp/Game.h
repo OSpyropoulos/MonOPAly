c219c7593a9e4b249fdbf3c6173170a51dac33ec
odysigmas <97303275+odysigmas@users.noreply.github.com>
2022-01-31 13:53:53 +0200

    Questionmarks & sound effects

--- Monopoly/ergasiacpp/Game.h
+++ Monopoly/ergasiacpp/Game.h
@@ -20,8 +20,9 @@ protected:
 	Player* next_player = nullptr;
 	Player* active_player = nullptr;
 
-	// 27 cards in total
+	// 28 cards in total
 	
+	// 20 asset Cards
 	// Brown ( region = 0 )
 	Asset brown1 = Asset(670, 479, "WC", 45, 0);
 	Asset brown2 = Asset(607, 479, "Elevator", 50, 0);
@@ -51,14 +52,22 @@ protected:
 	Asset blue1 = Asset(742, 350, "Amph_Derigni", 300, 7);
 	Asset blue2 = Asset(742, 415, "Amph_Antoniadou", 300, 7);
 
-	// 20 asset Cards
 	std::vector<Asset> assets = { brown1,brown2,lightblue1,lightblue2,lightblue3,pink1,pink2,orange1,orange2,orange3,red1,red2,red3,yellow1,yellow2,green1,green2,green3,blue1,blue2 };
 	// initialize the pointer
 	Asset* cur_asset = &assets[1];
 
-	// 4 questionmark Cards
-	//Questionmark questionmarks[4];
+	// AND
+	// 8 questionmark Cards
+	Questionmark q1 = Questionmark(538, 479, "You received Ioannis Kabouras Award GET 100 EUR", 100);
+	Questionmark q2 = Questionmark(258, 278, "OPA decides to clean the graffitis  PAY 50 EUR", -50);
+	Questionmark q3 = Questionmark(538, 22, "For the monthly meals of the other player  PAY 40 EUR", -40);
+	Questionmark q4 = Questionmark(742, 278, "Your parents are happy with your grades  GET 20 EUR", 20);
+	Questionmark q5 = Questionmark(538, 479, "IT needs more computers  PAY 50 EUR", -50);
+	Questionmark q6 = Questionmark(258, 278, "A student wants your help with his project  GET 80 EUR", 80);
+	Questionmark q7 = Questionmark(538, 22, "A student wants your notes, GET 30 EUR", 30);
+	Questionmark q8 = Questionmark(742, 278, "For your friends coffee  PAY 10 EUR ", -10);
 
+	std::vector<Questionmark> questionmarks = { q1,q2,q3,q4,q5,q6,q7,q8 };
 
 public:
 	
